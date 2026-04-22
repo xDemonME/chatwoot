26072052a32c37243f61cb64f265732c38c5a473
xdemonme <mail@xdemon.ru>
2026-04-22 18:02:26 +0700

    feat(whatsapp): store business account id on conversations

--- app/services/whatsapp/incoming_message_service_helpers.rb
+++ app/services/whatsapp/incoming_message_service_helpers.rb
@@ -77,19 +77,28 @@ module Whatsapp::IncomingMessageServiceHelpers
   end
 
   def whatsapp_conversation_additional_attributes
+    additional_attributes = {}
     referral_attributes = messages_data.first[:referral]&.to_h&.deep_stringify_keys
-    return {} if referral_attributes.blank?
+    additional_attributes['referral'] = referral_attributes if referral_attributes.present?
 
-    { 'referral' => referral_attributes }
+    waba_id = whatsapp_business_account_id
+    additional_attributes['whatsapp_business_account_id'] = waba_id if waba_id.present?
+
+    additional_attributes
   end
 
   def merge_conversation_additional_attributes!(conversation)
-    referral_attributes = whatsapp_conversation_additional_attributes
-    return if referral_attributes.blank?
+    additional_attributes = whatsapp_conversation_additional_attributes
+    return if additional_attributes.blank?
 
-    merged_attributes = (conversation.additional_attributes || {}).deep_merge(referral_attributes)
+    merged_attributes = (conversation.additional_attributes || {}).deep_merge(additional_attributes)
     return if merged_attributes == conversation.additional_attributes
 
     conversation.update!(additional_attributes: merged_attributes)
+  end
+
+  def whatsapp_business_account_id
+    entry = params[:entry]&.first || params['entry']&.first
+    entry&.[](:id) || entry&.[]('id')
   end
 end
